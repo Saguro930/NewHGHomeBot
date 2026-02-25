eea529a9eb59276d29d1576358c0cf84f07c6156
Sou930 <69160@g.hiroshimagakuin.ed.jp>
2026-02-25 18:26:25 +0900

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -3,7 +3,7 @@ print("🔥 keep_alive.py imported")
 from flask import Flask, send_from_directory
 import os
 from data.firebase_init import init_firebase
-from site.sns.sns import register_sns_routes
+from web.sns.sns import register_sns_routes
 
 app = Flask(__name__)
 db = init_firebase()
